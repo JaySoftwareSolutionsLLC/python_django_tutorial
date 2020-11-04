c82cb5d0e1c442f452ae74a9bf948f1dd1af842f
BrettBrewster <bbrewster@sealanddesign.com>
2020-11-04 12:20:39 -0500

    Python Django Tutorial: Part 3 (first 20 minutes)
    - Create templates folder
    - Create 2 template files
    - Create fake data (posts dictionary)
    - Update views to use render rather than HttpResponse
    - Add blog app to project settings file
    - Set blog as homepage

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Normally, we will pull this info in from db, but using hard-coded data for example data to be used with templating
+posts = [
+    {
+        'author': 'Brett',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'November 1, 2020'
+    },
+    {
+        'author': 'Brett',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'November 4, 2020'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'}) # Directly passing context
