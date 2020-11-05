08146e134aa3aa674c0915b87f0e5c227ca8853d
BrettBrewster <bbrewster@sealanddesign.com>
2020-11-05 18:46:59 -0500

    Python Django Tutorial: Part 5
    - Create a blog model
    - Make and run migration
    - Create & save posts via django-python terminal
    - Change blog views to query from DB
    - Update format of date_posted display
    - Register Post model inside admin page

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Post
 
-# Normally, we will pull this info in from db, but using hard-coded data for example data to be used with templating
-posts = [
-    {
-        'author': 'Brett',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'November 1, 2020'
-    },
-    {
-        'author': 'Brett',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'November 4, 2020'
-    }
-]
+posts = Post.objects.all()
 
 def home(request):
     context = {
