88a214dc8814353dfc5663d937b4068dad8a7111
BrettBrewster <bbrewster@sealanddesign.com>
2020-11-16 19:33:51 -0500

    Revert "Revert "Python Django Tutorial: Part 10""
    This reverts commit 8111b2312e0b2d7f13c72d6d426285eef78b025c.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from . models import Post
 
 def home(request):
@@ -11,9 +11,13 @@ def home(request):
 
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/home.html'
+    template_name = 'blog/home.html' # Default is <app>/<model>_<viewtype>.html
     context_object_name = 'posts' # Default is object_list?
     ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+    
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'}) # Directly passing context
